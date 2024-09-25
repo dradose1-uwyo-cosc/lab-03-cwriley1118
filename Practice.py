f51fafd93f80064190a66e457c97633de491c697
cwriley1118 <cwriley1118@gmail.com>
2024-09-25 21:47:51 +0000

    Completed all sections

---
+++ Practice.py
@@ -0,0 +1,11 @@
+names = ["Colton", "Tyler", "Braxston", "Colin", "Colter"]
+print(names)
+del names[0]
+print(names)
+names.append("Bob")
+print(names)
+for name in names:
+    print(f"[name] is on my table")
+names.reverse()
+print(names)
+
